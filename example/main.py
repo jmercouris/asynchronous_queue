6d91fc81b14ba3fc1c35a3213e72492d42c99d3f
John Mercouris <jmercouris@gmail.com>
2016-10-30 16:16:24 +0100

    Commented example showing steps and variable purposes

--- example/main.py
+++ example/main.py
@@ -2,27 +2,34 @@ from asynchronousqueue.asynchronous_queue import AsynchronousQueue as Queue
 from asynchronousqueue.task import Task
 import time
 
-parallelism = 2
-task_count = 5
+parallelism = 2  # How many simultaneous threads do we permit
+task_count = 5  # How many demonstration tasks should we create
 
 
 def function():
+    """ Sample Function to be executed as a Task
+    """
+    
     time.sleep(1)
     print('Executed Function')
 
 
 def callback():
+    """ Sample Function to be executed as a callback
+    """
     print('Received Callback')
 
 
-queue = Queue(parallelism)
+queue = Queue(parallelism) # Create our AsynchronousQueue
 
+# Generate a set of Tasks
 for i in range(0, task_count):
     queue.add_task(Task('Task {}'.format(i), function, callback))
 
+# Start executing the Tasks
 queue.start()
 
-
+# Wait until all tasks have been Executed
 while queue.is_running():
     pass
 
