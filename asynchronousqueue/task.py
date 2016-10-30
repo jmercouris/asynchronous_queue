ed06b5e1afd6c0316b2d6ba6f23c82f6694eb653
John Mercouris <jmercouris@gmail.com>
2016-10-30 16:20:08 +0100

    Sphinx doc commented task

--- asynchronousqueue/task.py
+++ asynchronousqueue/task.py
@@ -1,5 +1,10 @@
 class Task(object):
-    """Documentation for Task
+    """A Task object is a convenient container for a function and an associated
+    callback. It stores a couple of useful things:
+    
+    :ivar identifier: A string identifier that is passed to identify the task
+    :ivar function: The function to be executed
+    :ivar callback: The callback which will be invoked upon function execution
     
     """
     def __init__(self, identifier, function, callback):
@@ -9,6 +14,10 @@ class Task(object):
         self.callback = callback
     
     def execute(self):
+        """Execute the function passed in during the task creation, then invoke
+        the callback, and finally, notify the queue of the task completion
+        """
+        
         print('Executing Task: {}'.format(self.identifier))
         self.function()
         self.callback()
