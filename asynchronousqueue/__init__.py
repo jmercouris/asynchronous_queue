ab106ea965016c262923b0eb4804d84d777ca7c0
John Mercouris <jmercouris@gmail.com>
2016-10-30 13:30:58 +0100

    Mini queue implementation created to avoid using python queue library

--- asynchronousqueue/__init__.py
+++ asynchronousqueue/__init__.py
@@ -4,23 +4,43 @@ class AsynchronousQueue(object):
     """
     def __init__(self, parallelism):
         super(AsynchronousQueue, self).__init__()
+        self.local_queue = LocalQueue()
+        
         print('Hello World')
     
-    def start():
+    def start(self):
         pass
     
-    def size():
+    def size(self):
         pass
     
-    def is_running():
+    def is_running(self):
         pass
     
-    def in_flight():
+    def in_flight(self):
         pass
     
-    def add_task():
+    def add_task(self):
         pass
     
-    def add_callback():
+    def add_callback(self):
         pass
+
+
+class LocalQueue(object):
+    """Documentation for Queue
+    
+    """
+    def __init__(self):
+        super(LocalQueue, self).__init__()
+        self.elements = []
+    
+    def enqueue(self, element):
+        self.elements.insert(0, element)
+    
+    def dequeue(self):
+        return self.elements.pop()
+
+
+
 
