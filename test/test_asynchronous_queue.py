fcded8b2f70f619a96646e50bce09735b9efe927
John Mercouris <jmercouris@gmail.com>
2016-10-30 19:12:19 +0100

    Added basic task testing, working on asynchronous queue

--- test/test_asynchronous_queue.py
+++ test/test_asynchronous_queue.py
@@ -1,11 +1,33 @@
 from asynchronousqueue.asynchronous_queue import AsynchronousQueue as Queue
 from asynchronousqueue.task import Task
-import time
-
-parallelism = 2  # How many simultaneous threads do we permit
-task_count = 5  # How many demonstration tasks should we create
+import pytest
 
 
-def test_is_running():
+def function():
+    """ Sample Function to be executed as a Task
+    """
+    pass
+
+
+def callback():
+    """ Sample Function to be executed as a callback
+    """
+    pass
+
+
+@pytest.fixture
+def aq():
+    parallelism = 4  # How many simultaneous threads do we permit
     queue = Queue(parallelism)
-    assert queue.is_running() == False
+    return queue
+
+
+def test_aq_terminates(aq):
+    # Generate a set of Tasks
+    task_count = 5  # How many demonstration tasks should we create
+    for i in range(0, task_count):
+        aq.add_task(Task('Task {}'.format(i), function, callback))
+    aq.start()
+
+
+
--- test/test_task.py
+++ test/test_task.py
@@ -1,7 +1,25 @@
 from asynchronousqueue.task import Task
+from asynchronousqueue.asynchronous_queue import AsynchronousQueue
+
+
+def test_function():
+    pass
+
+
+def test_callback():
+    pass
 
 
 def test_task_creation():
     """ Test that tasks can be created
     """
-    Task()
+    Task('Identifier', test_function, test_callback)
+
+
+def test_task_execution():
+    """ Test that tasks can be created and executed
+    """
+    aq = AsynchronousQueue(1)
+    t = Task('Identifier', test_function, test_callback)
+    aq.add_task(t)
+    t.execute()
