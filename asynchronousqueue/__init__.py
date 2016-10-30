cfef9bf1ed21896c3f4ef022f188775667982abe
John Mercouris <jmercouris@gmail.com>
2016-10-30 12:45:05 +0100

    Added method stubs to asynchronous queue class

--- asynchronousqueue/__init__.py
+++ asynchronousqueue/__init__.py
@@ -4,6 +4,23 @@ class Queue(object):
     """
     def __init__(self, parallelism):
         super(Queue, self).__init__()
-        print('Hello World')\
-        
+        print('Hello World')
+    
+    def start():
+        pass
+    
+    def size():
+        pass
+    
+    def is_running():
+        pass
+    
+    def in_flight():
+        pass
+    
+    def add_task():
+        pass
+    
+    def add_callback():
+        pass
 
